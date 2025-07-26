41c6b8e208c356bb4d7eb96545b1eae17ec6e174
fumasuga <f0976917@gmail.com>
2025-07-26 17:50:52 +0900

    change components.intro

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -1,4 +1,7 @@
-import { CMS_NAME } from "@/lib/constants";
+let today = new Date();
+let formattedDate = `${today.getMonth() + 1}.${today.getDate()}`;
+let formattedYear = today.getFullYear();
+let weekday = today.toLocaleDateString('en-US', {weekday: 'long'});
 
 export function Intro() {
   return (
@@ -6,16 +9,21 @@ export function Intro() {
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         Blog.
       </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{" "}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-blue-600 duration-200 transition-colors"
-        >
-          Next.js
-        </a>{" "}
-        and {CMS_NAME}.
-      </h4>
+      <div className="flex text-center md:text-left text-lg mt-5 md:pl-8">
+        <p className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+          {formattedDate}
+        </p>
+        <div className="text-center text-lg mt-5">
+          <p
+            className="text-gray-600 md:text-right"
+          >
+            {formattedYear}
+          </p>{" "}
+          <p className="text-gray-600 md:text-right">
+            {weekday}
+          </p>
+        </div>
+      </div>
     </section>
   );
 }
