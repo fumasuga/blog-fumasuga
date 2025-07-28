5cb1a93bcd706d2ff75de659d4e8d13f76756866
fumasuga <f0976917@gmail.com>
2025-07-28 23:25:57 +0900

    support responsive components.intro.tsx

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -5,21 +5,19 @@ let weekday = today.toLocaleDateString('en-US', {weekday: 'long'});
 
 export function Intro() {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+    <section className="flex flex-col md:flex-row items-center md:justify-between mt-8 mb-8 md:mt-16 md:mb-12 px-4 md:px-0">
+      <h1 className="text-4xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8 mb-4 md:mb-0">
         Blog.
       </h1>
-      <div className="flex text-center md:text-left text-lg mt-5 md:pl-8">
-        <p className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+      <div className="flex flex-row items-center">
+        <p className="text-4xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-4 md:mb-2 md:mb-0">
           {formattedDate}
         </p>
-        <div className="text-center text-lg mt-5">
-          <p
-            className="text-gray-600 md:text-right"
-          >
+        <div className="flex flex-col items-center justify-center text-center text-lg ml-4">
+          <p className="text-gray-600 text-base md:text-lg">
             {formattedYear}
-          </p>{" "}
-          <p className="text-gray-600 md:text-right">
+          </p>
+          <p className="text-gray-600 text-base md:text-lg">
             {weekday}
           </p>
         </div>
