94f06f7ac6b167a49e0f16b669c43ca6f03bbcd2
fumasuga <f0976917@gmail.com>
2025-07-28 23:42:47 +0900

    fix layout components.intro.tsx

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -5,21 +5,25 @@ let weekday = today.toLocaleDateString('en-US', {weekday: 'long'});
 
 export function Intro() {
   return (
-    <section className="flex flex-col md:flex-row items-center md:justify-between mt-8 mb-8 md:mt-16 md:mb-12 px-4 md:px-0">
-      <h1 className="text-4xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8 mb-4 md:mb-0">
-        Blog.
-      </h1>
-      <div className="flex flex-row items-center">
-        <p className="text-4xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-4 md:mb-2 md:mb-0">
-          {formattedDate}
-        </p>
-        <div className="flex flex-col items-center justify-center text-center text-lg ml-4">
-          <p className="text-gray-600 text-base md:text-lg">
-            {formattedYear}
+    <section className="flex flex-col md:flex-row items-center md:justify-between mt-6 mb-6 md:mt-12 md:mb-10 px-2 md:px-0">
+      <div className="flex flex-row justify-between items-center w-full">
+        <div>
+          <h1 className="text-2xl md:text-6xl font-bold tracking-tighter leading-tight mb-0">
+            Blog.
+          </h1>
+        </div>
+        <div className="flex flex-row items-center">
+          <p className="text-2xl md:text-6xl font-bold tracking-tighter leading-tight md:pr-2 mb-0">
+            {formattedDate}
           </p>
-          <p className="text-gray-600 text-base md:text-lg">
-            {weekday}
-          </p>
+          <div className="flex flex-col items-end justify-center text-right ml-2 md:ml-4">
+            <p className="text-gray-600 text-xs md:text-sm">
+              {formattedYear}
+            </p>
+            <p className="text-gray-600 text-xs md:text-sm">
+              {weekday}
+            </p>
+          </div>
         </div>
       </div>
     </section>
